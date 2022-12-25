b9c5a4abb38e60e265d37583d9dfc2218617917b
MSC BEEHI <112796660+MSCBEEHI@users.noreply.github.com>
2022-12-25 19:01:55 +0700

    main

--- src/Rank Chat/rank-chat.ts
+++ src/Rank Chat/rank-chat.ts
@@ -6,6 +6,6 @@ mc.world.events.beforeChat.subscribe((eventData) => {
     const rank = player.getTags().filter(t => t.startsWith('rank:')).map(p => `[${p.split(':')[1]}§r]`).join('')
     const msg = eventData.message
     eventData.cancel = true
-    if (rank[0]) player.tell(`${rank}§7${player.name}:§r ${msg}`)
-    else player.tell(`[§7MEMBER§r]§7${player.name}:§r ${msg}`)
+    if (rank[0]) mc.world.say(`${rank}§7${player.name}:§r ${msg}`)
+    else mc.world.say(`[§7MEMBER§r]§7${player.name}:§r ${msg}`)
 })
