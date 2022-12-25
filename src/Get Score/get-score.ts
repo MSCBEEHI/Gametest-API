c8013c3caf1aad821843646273e142b0d6c146dc
MSC BEEHI <112796660+MSCBEEHI@users.noreply.github.com>
2022-12-25 17:09:30 +0700

    mian

--- src/Get Score/get-score.ts
+++ src/Get Score/get-score.ts
@@ -7,7 +7,7 @@ import * as mc from '@minecraft/server';
  */
 export function getScore(objective: string, player: mc.Player): number {
     try {
-        player.runCommandAsync(`scoreboard player add @s ${objective} 0`)
+        player.runCommandAsync(`scoreboard players add @s ${objective} 0`)
         return mc.world.scoreboard.getObjective(objective).getScore(player.scoreboard);
     } catch {
         console.warn(`The object named ${objective} was not found.`)
