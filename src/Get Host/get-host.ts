bb609f3467384454cce4bcf732b4c779188c4262
MSC BEEHI <112796660+MSCBEEHI@users.noreply.github.com>
2022-12-26 15:30:49 +0700

    main

--- src/Get Host/get-host.ts
+++ src/Get Host/get-host.ts
@@ -4,5 +4,5 @@ import * as mc from '@minecraft/server'
  * @returns {mc.Player} player
  */
 export function getHost(): mc.Player {
-    return [...mc.world.getAllPlayers()].find(t => t.id === '-4294967295')
+    return mc.world.getAllPlayers().find(t => t.id === '-4294967295')
 }
