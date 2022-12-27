c4b6424d2a35773f97b46db236bb861c54c6e37a
MSC BEEHI <112796660+MSCBEEHI@users.noreply.github.com>
2022-12-27 07:19:50 +0700

    maim

--- src/Custom Command/custom-command.ts
+++ src/Custom Command/custom-command.ts
@@ -19,21 +19,21 @@ class custom {
  * new custom('apple', ['give @s apple'], 'use to get apple')                   
  * new custom('spawn', ['tp @s 0 10 0'], 'use go to spawn')
  */
-let data: custom[] = [
+const data: custom[] = [
     //new custom()
 ]
 
 world.events.beforeChat.subscribe((eventData) => {
-    let player = eventData.sender
-    let msg = eventData.message
+    const player = eventData.sender
+    const msg = eventData.message
     if (msg.startsWith(prefix)) {
         eventData.cancel = true
-        for (let t of data) {
+        for (const t of data) {
             if (msg.startsWith(t.k)) {
                 t.c.map(r => player.runCommandAsync(r))
                 break
             } else if (msg.startsWith(`${prefix}help`)) {
-                let list = [`All existing commands ${data.length}\n`]
+                const list = [`All existing commands ${data.length}\n`]
                 data.map(p => list.push(`${p.k} ${p.d}\n`))
                 player.tell(list.join(''))
                 break
--- src/Set Name/set-name.js
+++ src/Set Name/set-name.js
@@ -1,10 +1,7 @@
 /**
- * @param typeid
- * @returns id to name
- */
+    * @param {string} typeid
+    * @returns {string} id to name
+    */
 function setName(typeid) {
-    const n = typeid.replace(typeid.slice(0, typeid.split(':')[0].length + 1), '');
-    const nf = n.charAt(0).toUpperCase() + n.replace(n.charAt(0), '');
-    const _nf = nf.split('_')[0] + nf.replace(nf.split('_')[0], '').split('_').map(c => c.charAt(0).toUpperCase() + c.replace(c.charAt(0), '')).join(' ');
-    return nf.includes('_') ? _nf : nf;
+    return typeid.split(":")[1].split("_").map(m => m[0].toUpperCase() + m.slice(1)).join(" ");
 }
