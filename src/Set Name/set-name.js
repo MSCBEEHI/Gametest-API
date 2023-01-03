803d640eabe1767fe2506fc1bc483710f6ed54e3
MSC BEEHI <112796660+MSCBEEHI@users.noreply.github.com>
2023-01-03 07:25:33 +0700

    main

--- src/Set Name/set-name.js
+++ src/Set Name/set-name.js
@@ -2,6 +2,6 @@
     * @param {string} typeid
     * @returns {string} id to name
     */
-function setName(typeid) {
+export function setName(typeid) {
     return typeid.split(":")[1].split("_").map(m => m[0].toUpperCase() + m.slice(1)).join(" ");
 }
